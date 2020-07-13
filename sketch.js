4f37bb3c43a3329b0522795d715995349497f24d
TheSnickersBar <39471794+TheSnickersBar@users.noreply.github.com>
2020-07-13 11:01:03 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var database;
+var line1 = [];
+var allPoints;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    this.line = null;
+    
+    
+}
+
+function draw(){
+    background(0);
+    if (mouseIsPressed){
+      var point = {
+        x: mouseX, 
+        y: mouseY
+      }
+      line1.push(point);
+    }
+
+    beginShape();
+    stroke(255);
+    noFill();
+    for(var i=0; i<this.line1.length; i++){     
+        vertex(line1[i].x, line1[i].y);
+        update(line1[i].x, line1[i].y);
+      }
+    endShape();
+    extractInfo();
+   
+    
+
+  
+}
+
+function update(x,y){ 
+database.ref('/').set({
+  x: x,
+  y: y
+
+})
+
+function extractInfo(){
+  point = data.val();
+  line1[i].x = point.x;
+  line1[i].y = point.y;
+    
+}
+
+   
+
+}
